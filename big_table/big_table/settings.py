55b28f46f1b426b91a1789edd2665763810df928
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-04-15 23:01:42 +0300

    Update settings.py

--- big_table/big_table/settings.py
+++ big_table/big_table/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'table.apps.TableConfig',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'catalog.apps.TableConfig',
             ],
         },
     },
