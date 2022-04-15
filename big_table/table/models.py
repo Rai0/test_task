0fac4da5dfbbb64ecca7d24669c9e65e00fc15e8
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-04-15 23:00:25 +0300

    create modals

--- big_table/table/models.py
+++ big_table/table/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class tabelModel (models.Model):
+
+    date = models.DateTimeField (default= "", blank= False)
+    name = models.CharField (max_length= 256, unique= True, blank= False, help_text= "Enter name of the field", error_messages= "You fogote enter name", db_index= True)
+    number = models.IntegerField (null= True ,blank= True, db_index= True)
+    distance = models.FloatField (null= False ,blank= True, db_index= True)
+
+    def __str__(self) -> str:
+        return self.name
