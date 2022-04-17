b554807cbf571588dd10082b09266b77e613b7a4
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-04-17 16:44:49 +0300

    chnge models

--- big_table/table/models.py
+++ big_table/table/models.py
@@ -7,7 +7,7 @@ class tabelModel (models.Model):
     title = models.CharField (verbose_name= "Название", max_length= 256, unique= True, blank= False, help_text= "Enter title for this field", db_index= True)
     number = models.IntegerField (verbose_name= "Количество", default= 1, null= True, blank= True, help_text= "Number of something", db_index= True)
     distance = models.DecimalField (verbose_name= "Расстояние", default= 0,  help_text= "Distance to something", db_index= True, max_digits= 10, decimal_places= 2)
-    units = models.CharField (verbose_name= "Единицы измерения", max_length= 10, blank= True, help_text= "Length units", default= "km", choices= [("km", "km"), ("m", "m"), ("cm", "cm"), ("mm", "mm")],db_index= True)
+    # units = models.CharField (verbose_name= "Единицы измерения", max_length= 10, blank= True, help_text= "Length units", default= "km", choices= [("km", "km"), ("m", "m"), ("cm", "cm"), ("mm", "mm")],db_index= True)
 
     class Meta:
         verbose_name = 'Cтрока таблицы'
