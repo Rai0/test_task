b64a46e924847407bfbab557c820d46bda6445a9
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-04-18 06:05:01 +0300

    change vuejs to vue/sli

--- big_table/big_table/settings.py
+++ big_table/big_table/settings.py
@@ -31,7 +31,7 @@ if os.path.exists(dotenv_path):
 SECRET_KEY = os.environ.get ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
