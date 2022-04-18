c157a2b9a472e7281b12904b31d06578951ce8aa
Rai0 <43114095+Rai0@users.noreply.github.com>
2022-04-18 19:21:53 +0300

    commit

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import GetData from './components/GetData.jsx';
+
+function App() {
+    return (
+            <GetData />
+    )
+}
+
+export default App;
